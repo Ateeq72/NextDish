f477b30933433932480f9cef9ad843d526604554
Ateeq Ahmed <ahmedateeq64@gmail.com>
2022-11-30 21:20:52 +0530

    init app

---
+++ app/Controllers/Http/SupabasesController.ts
@@ -0,0 +1,12 @@
+// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+import { SupabaseClient } from "@supabase/supabase-js";
+
+export default class SupabasesController {
+
+  public async getData({ view }) {
+
+    return view.render('welcome');
+
+  }
+}
