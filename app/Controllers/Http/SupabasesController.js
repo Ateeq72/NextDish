c18dc7c03f609a6369e3494f71b69e20c591c04d
Ateeq Ahmed <ahmedateeq64@gmail.com>
2022-12-02 15:22:11 +0530

    built

---
+++ app/Controllers/Http/SupabasesController.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class SupabasesController {
+    async getData({ view }) {
+        return view.render('welcome');
+    }
+}
+exports.default = SupabasesController;
+//# sourceMappingURL=SupabasesController.js.map
---
+++ start/routes.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const Route_1 = __importDefault(global[Symbol.for('ioc.use')]("Adonis/Core/Route"));
+Route_1.default.get('/', async ({ view }) => {
+    return view.render('welcome');
+});
+Route_1.default.get('/test', async ({}) => {
+    return { status: 'success' };
+});
+Route_1.default.get('/supabase', 'SupabasesController.getData');
+//# sourceMappingURL=routes.js.map
