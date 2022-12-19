5e60d2a6c4f94a61d2348228a4133f90e0c5e800
Ateeq Ahmed <ahmedateeq64@gmail.com>
2022-12-19 21:29:38 +0530

    chanegs for assets

--- start/routes.ts
+++ start/routes.ts
@@ -20,12 +20,73 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
+
 Route.get('/', async ({ view }) => {
   return view.render('index')
 })
 
-Route.get('/test', async ({  }) => {
-  return { status: 'success' };
+Route.get('/index', async ({ view }) => {
+  return view.render('index')
+})
+
+Route.get('/basic-table', async ({ view }) => {
+  return view.render('basic-table')
+})
+
+Route.get('/buttons', async ({ view }) => {
+  return view.render('buttons')
+})
+
+Route.get('/calendar', async ({ view }) => {
+  return view.render('calendar')
+})
+
+Route.get('/charts', async ({ view }) => {
+  return view.render('charts')
+})
+
+Route.get('/chat', async ({ view }) => {
+  return view.render('chat')
+})
+
+Route.get('/compose', async ({ view }) => {
+  return view.render('compose')
+})
+
+Route.get('/datatable', async ({ view }) => {
+  return view.render('datatable')
+})
+
+Route.get('/email', async ({ view }) => {
+  return view.render('email')
+})
+
+Route.get('/forms', async ({ view }) => {
+  return view.render('forms')
+})
+
+Route.get('/google-maps', async ({ view }) => {
+  return view.render('google-maps')
+})
+
+Route.get('/signin', async ({ view }) => {
+  return view.render('signin')
+})
+
+Route.get('/signup', async ({ view }) => {
+  return view.render('signup')
+})
+
+Route.get('/ui', async ({ view }) => {
+  return view.render('ui')
+})
+
+Route.get('/vector-maps', async ({ view }) => {
+  return view.render('vector-maps')
+})
+
+Route.get('/test', async ({ view }) => {
+  return view.render('test')
 })
 
 Route.get('/supabase', 'SupabasesController.getData')
